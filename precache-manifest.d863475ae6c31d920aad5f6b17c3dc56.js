6978bfb43ead8373b787ca30d4af06d5d122de6d
calvinvadivelu <cvadivelu@gmail.com>
2019-11-03 01:39:59 -0400

    Updates

---
+++ precache-manifest.d863475ae6c31d920aad5f6b17c3dc56.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c22a73875f1ac79b435fab809d413c0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "82c217f316ebbdfc20c3",
+    "url": "/monsters-rolodex/static/css/main.bddc137f.chunk.css"
+  },
+  {
+    "revision": "82128c5ae226b744f7d0",
+    "url": "/monsters-rolodex/static/js/2.75d5cf1d.chunk.js"
+  },
+  {
+    "revision": "82c217f316ebbdfc20c3",
+    "url": "/monsters-rolodex/static/js/main.ddfc3601.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
